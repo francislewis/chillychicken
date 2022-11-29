0467a5db3b6b8da851f7b7fa5f729bbf2739407b
Francis <fran@lovewales.net>
2022-11-29 10:27:44 +0000

    OMP Parallel code

--- fft_omp_v1.c
+++ fft_omp_v1.c
@@ -10,7 +10,7 @@
 #define PI acos(-1)
 
 // Input pointer
-double complex* xinput = NULL;
+double complex* input = NULL;
 
 // Compute the even and odd parts of the FFT
 void even_odd(int size, int k, double complex* Ek, double complex* Ok){
@@ -20,16 +20,11 @@ void even_odd(int size, int k, double complex* Ek, double complex* Ok){
     *Ek=0+0*I;
     *Ok=0+0*I;
 
-    double complex exponent;
-
     for(int m = 0; m <= size/2 - 1; m++)
     {
-        // Find exponent term
-        exponent = cos(-2.0 * PI * m * k / (size/2)) + sin(-2.0 * PI * m * k / (size/2))*I;
-
         // Update odd and even parts
-        *Ek = *Ek + (xinput[2*m]*exponent);
-        *Ok = *Ok + (xinput[2*m+1]*exponent);
+        *Ek = *Ek + (input[2*m]*(cos(-2.0 * PI * m * k / (size/2)) + sin(-2.0 * PI * m * k / (size/2))*I));
+        *Ok = *Ok + (input[2*m+1]*(cos(-2.0 * PI * m * k / (size/2)) + sin(-2.0 * PI * m * k / (size/2))*I));
     }
 }
 
@@ -52,23 +47,24 @@ int main(int argc, char* argv[]){
     int dimension = atoi(argv[2]);
     int threads = atoi(argv[3]);
 
-    // Set number of omp threads
-    omp_set_num_threads(threads);
-
-    double start_time, end_time;
-
-    start_time = omp_get_wtime();
-
     // Check if dimension in [1,2] using DeMorgan
     if (!(dimension>0 && dimension<3)){
         printf("Number of dimensions can only be 1 or 2\n");
         return 1;
     }
 
+    // Set number of omp threads
+    omp_set_num_threads(threads);
+
+    printf("Size: %d, Dimension: %d, Threads: %d\n", size, dimension, threads);
+
+    double start_time, end_time;
+    start_time = omp_get_wtime();
+
     // Allocate memory for input values
-    // xinput is the 1-D array passed through FFT
+    // input is the 1-D array passed through FFT
     // Once for 1-D and multiple times with different data for 2D
-    xinput = malloc(size * sizeof(complex));
+    input = malloc(size * sizeof(complex));
 
     double complex* temp_results = NULL;
     temp_results = malloc(size * sizeof(complex));
@@ -80,14 +76,14 @@ int main(int argc, char* argv[]){
     // Seed random number with current time
     srand(time(NULL));
 
-	printf("Size: %d, Dimension: %d, Threads: %d\n", size, dimension, threads);
-
 	if(dimension==1){
 	    // Generate random input values
+	    #pragma omp parallel for
 	    for(int i = 0; i < size; i++){
-            xinput[i] = (((int) rand())%1000)+(((int) rand())%1000)*I;
+            input[i] = (((int) rand())%1000)+(((int) rand())%1000)*I;
+            //printf("inputdat1[%d] = %f+%fj\n",i,creal(input[i]),cimag(input[i]));
 	    }
-
+        #pragma omp parallel for
         for(int k = 0; k < size; k++){
             // Find the twiddle factor term
             twiddle_term = find_twiddle_term(size, k);
@@ -100,6 +96,7 @@ int main(int argc, char* argv[]){
 
             // Store result
             temp_results[k] = result;
+            //printf("%d: %f + %fj\n",k,creal(temp_results[k]),cimag(temp_results[k]));
         }
         end_time = omp_get_wtime();
         printf("Time: %8.6f s\n", end_time-start_time);
@@ -112,9 +109,9 @@ int main(int argc, char* argv[]){
         gsl_matrix_complex *rand_matrix = NULL;
         // Allocate memory for GSL matrix
         rand_matrix = gsl_matrix_complex_alloc(size, size);
-        // gsl_matrix_complex_set_all(rand_matrix, GSL_COMPLEX_ONE);
 
         // Fill matrix with complex values
+        #pragma omp parallel for collapse(2)
         for(int row = 0; row < size; row++){
             for(int col = 0; col < size; col++){
                 int rand1 =(((int) rand())%1000);
@@ -126,59 +123,68 @@ int main(int argc, char* argv[]){
             }
         }
 
+//        // Print to check
+//        printf("start:\n");
+//        for(int k = 0; k < size; k++){
+//            for(int j = 0; j < size; j++){
+//                printf("inputdat[%d][%d]= %f + %fj\n", k, j, GSL_REAL(gsl_matrix_complex_get(rand_matrix, k, j)), GSL_IMAG(gsl_matrix_complex_get(rand_matrix, k, j)));
+//            }
+//        }
+        printf("\n");
+
         // Loop over each row of matrix and do FFT
         for (int row=0; row < size; row++){
-            // Put data into xinput for FFT along this row
+            // Put data into input for FFT along this row
             for(int i = 0; i < size; i++){
-                xinput[i] = GSL_REAL(gsl_matrix_complex_get(rand_matrix, row, i))+GSL_IMAG(gsl_matrix_complex_get(rand_matrix, row, i))*I;
+                input[i] = GSL_REAL(gsl_matrix_complex_get(rand_matrix, row, i))+GSL_IMAG(gsl_matrix_complex_get(rand_matrix, row, i))*I;
+	            // would be good to include this in loop below and do a loop collapse
+	            // But getting wrong results when replacing i with col
 	        }
+            #pragma omp parallel for
             for (int col=0; col < size; col++){
+                //input[col] = GSL_REAL(gsl_matrix_complex_get(rand_matrix, row, col))+GSL_IMAG(gsl_matrix_complex_get(rand_matrix, row, col))*I;
                 // Do FFT
                 twiddle_term = find_twiddle_term(size, col);
                 even_odd(size, col, Ek, Ok);
                 result = *Ek + (twiddle_term* *Ok);
-                temp_results[col] = result;
-
-                // If at end of row, then temp_results is full and we need to save it
-                if (col == size-1){
-                    for (int i = 0; i < size; i++){
-                        gsl_complex temp_fill = gsl_complex_rect(creal(temp_results[i]), cimag(temp_results[i]));
-                        gsl_matrix_complex_set(rand_matrix, row, i, temp_fill);
-                    }
-                }
+                // Update matrix with result
+                gsl_matrix_complex_set(rand_matrix, row, col, gsl_complex_rect(creal(result), cimag(result)));
             }
         }
 
+        #pragma omp critical
         // Transpose matrix (in place - thanks to GSL)
         gsl_matrix_complex_transpose(rand_matrix);
 
         // Loop over each column (although accessing by row due to transpose)
         for (int col=0; col < size; col++){
-            // Put data into xinput for FFT along this row
+            // Put data into input for FFT along this row
             for(int i = 0; i < size; i++){
-                xinput[i] = GSL_REAL(gsl_matrix_complex_get(rand_matrix, col, i))+GSL_IMAG(gsl_matrix_complex_get(rand_matrix, col, i))*I;
+                input[i] = GSL_REAL(gsl_matrix_complex_get(rand_matrix, col, i))+GSL_IMAG(gsl_matrix_complex_get(rand_matrix, col, i))*I;
 	        }
+	        #pragma omp parallel for
             for (int row=0; row < size; row++){
                 // Do FFT
                 twiddle_term = find_twiddle_term(size, row);
                 even_odd(size, row, Ek, Ok);
                 result = *Ek + (twiddle_term* *Ok);
-                temp_results[row] = result;
-
-                // If at end of col, then temp_results is full and we need to save it
-                if (row == size-1){
-                    for (int i = 0; i < size; i++){
-                        gsl_complex temp_fill = gsl_complex_rect(creal(temp_results[i]), cimag(temp_results[i]));
-                        gsl_matrix_complex_set(rand_matrix, col, i, temp_fill);
-                    }
-                }
+                gsl_matrix_complex_set(rand_matrix, col, row, gsl_complex_rect(creal(result), cimag(result)));
             }
         }
 
+        #pragma omp critical
         // Transpose matrix back for final results, stored in rand_matrix
         gsl_matrix_complex_transpose(rand_matrix);
         end_time = omp_get_wtime();
         printf("Time: %8.6f s\n", end_time-start_time);
+
+//        // Print to check
+//        for(int k = 0; k < size; k++){
+//            for(int j = 0; j < size; j++){
+//                printf("%d %d : %f + %f j\n", k, j, GSL_REAL(gsl_matrix_complex_get(rand_matrix, k, j)), GSL_IMAG(gsl_matrix_complex_get(rand_matrix, k, j)));
+//            }
+//        }
+        printf("\n");
 	}
     return 0;
 }
